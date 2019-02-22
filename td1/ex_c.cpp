b3eaa953691aabd963e51046bb5a34bdf3267f27
cloune <rachel@nebulae.co>
2019-02-22 17:05:48 +0100

    exc-doesnotwork

--- td1/ex_c.cpp
+++ td1/ex_c.cpp
@@ -23,11 +23,9 @@ void toggleStopValue(int sig, siginfo_t* si, void*)
 	*ptr =true;
 }
 
-void exC()
+void calib (double* a, double* b)
 {
-    double counter = 0.;
-    volatile bool stop = false; 
-    struct timespec debut, fin, duree ;
+    double t1, n1, t2, n2;
     struct sigaction sa ;
 	sa.sa_flags=SA_SIGINFO ;
 	sa.sa_sigaction = toggleStopValue;
@@ -36,21 +34,50 @@ void exC()
 	struct sigevent sev ;
 	sev.sigev_notify = SIGEV_SIGNAL ;
 	sev.sigev_signo = SIGRTMIN ;
-	sev.sigev_value.sival_ptr = &stop ;
+	sev.sigev_value.sival_ptr = (void*) &stop ;
 	timer_t tid;
 	timer_create(CLOCK_REALTIME, &sev, &tid);
 	itimerspec its;
-	its.it_value.tv_sec = 1; 
-	its.it_value.tv_nsec = 0; 
+    t1=4000.;
+    n1=executeFunctionForGivenTime(timespec_from_ms(t1),tid,its);
+    t2=6000;
+    n2=executeFunctionForGivenTime(timespec_from_ms(t2),tid,its);
+    *a=(n2-n1)/(t2-t1);
+    *b=n2-(*a)*t2;
+    timer_delete(tid);
+}
+
+int executeFunctionForGivenTime(struct timespec ts, timer_t tid, itimerspec its)
+{
+    double counter = 0.;
+    volatile bool stop = false; 
+	its.it_value.tv_sec = ts.tv_sec; 
+	its.it_value.tv_nsec = ts.tv_nsec; 
 	its.it_interval.tv_sec = 0; 
 	its.it_interval.tv_nsec = 0; 
 	timer_settime(tid,0,&its,NULL);
-    clock_gettime(CLOCK_REALTIME, &debut);
     unsigned int loops = incr(UINT_MAX, &counter, &stop);
+    return loops;
+
+}
+
+void exC()
+{
+    double counter = 0.;
+    volatile bool stop = false; 
+
+    double a=0;
+    double b=0;
+    calib(&a,&b);
+    struct timespec debut, fin, duree ;
+
+    std::cout << "a = " << a <<" b = " << b << ".\n";
+
+    clock_gettime(CLOCK_REALTIME, &debut);
+    incr(22000, &counter, &stop);
     clock_gettime(CLOCK_REALTIME, &fin);
-    timer_delete(tid);
-    duree = fin - debut;
-    std::cout << "Durée d'execution: " << timespec_to_ms(duree) << ".\n";
-    std::cout << "Nombre de boucles: " << loops << ".\n";
+
+    duree=fin-debut;
+    std::cout << "duree = " << timespec_to_ms(duree) <<" nombre suppose de boucle  = " << a*timespec_to_ms(duree)+b << ".\n";
 
 }
