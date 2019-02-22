d3a7c47318f64b277e1cbaa0e480e3c26ffb1eed
cloune <rachel@nebulae.co>
2019-02-22 16:09:28 +0100

    mesure time of exec

--- td1/ex_b.cpp
+++ td1/ex_b.cpp
@@ -1,6 +1,7 @@
 #include "ex_b.h"
-
+#include "timer.h"
 #include <cstdlib>
+#include <iostream>
 
 void incr(unsigned int nLoops, double* pCounter)
 {
@@ -11,6 +12,11 @@ void incr(unsigned int nLoops, double* pCounter)
 void exB(int argc, char* argv[])
 {
     double counter = 0.;
-
+    struct timespec debut, fin, duree ;
+    
+    clock_gettime(CLOCK_REALTIME, &debut);
     incr(std::atoi(argv[1]), &counter);
+    clock_gettime(CLOCK_REALTIME, &fin);
+    duree = fin - debut;
+    std::cout << "Durée d'execution: " << timespec_to_ms(duree) << ".\n";
 }
