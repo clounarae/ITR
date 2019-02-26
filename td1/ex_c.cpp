306574d2bc8c6981646e68142473938b35073b90
cloune <rachel@nebulae.co>
2019-02-26 08:54:56 +0100

    exo c

--- td1/ex_c.cpp
+++ td1/ex_c.cpp
@@ -23,34 +23,9 @@ void toggleStopValue(int sig, siginfo_t* si, void*)
 	*ptr =true;
 }
 
-void calib (double* a, double* b)
-{
-    double t1, n1, t2, n2;
-    struct sigaction sa ;
-	sa.sa_flags=SA_SIGINFO ;
-	sa.sa_sigaction = toggleStopValue;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGRTMIN, &sa, NULL);
-	struct sigevent sev ;
-	sev.sigev_notify = SIGEV_SIGNAL ;
-	sev.sigev_signo = SIGRTMIN ;
-	sev.sigev_value.sival_ptr = (void*) &stop ;
-	timer_t tid;
-	timer_create(CLOCK_REALTIME, &sev, &tid);
-	itimerspec its;
-    t1=4000.;
-    n1=executeFunctionForGivenTime(timespec_from_ms(t1),tid,its);
-    t2=6000;
-    n2=executeFunctionForGivenTime(timespec_from_ms(t2),tid,its);
-    *a=(n2-n1)/(t2-t1);
-    *b=n2-(*a)*t2;
-    timer_delete(tid);
-}
-
-int executeFunctionForGivenTime(struct timespec ts, timer_t tid, itimerspec its)
+int executeFunctionForGivenTime(struct timespec ts, timer_t tid, itimerspec its, volatile bool stop)
 {
     double counter = 0.;
-    volatile bool stop = false; 
 	its.it_value.tv_sec = ts.tv_sec; 
 	its.it_value.tv_nsec = ts.tv_nsec; 
 	its.it_interval.tv_sec = 0; 
@@ -61,10 +36,37 @@ int executeFunctionForGivenTime(struct timespec ts, timer_t tid, itimerspec its)
 
 }
 
+void calib (double* a, double* b)
+{
+    double t1, n1, t2, n2;
+    struct sigaction sa ;
+	sa.sa_flags=SA_SIGINFO ;
+	sa.sa_sigaction = toggleStopValue;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGRTMIN, &sa, NULL);
+	struct sigevent sev ; 
+    volatile bool stop = false; 
+	sev.sigev_notify = SIGEV_SIGNAL ;
+	sev.sigev_signo = SIGRTMIN ;
+	sev.sigev_value.sival_ptr = (void*) &stop ;
+	timer_t tid;
+	timer_create(CLOCK_REALTIME, &sev, &tid);
+	itimerspec its;
+    t1=4000.;
+    n1=executeFunctionForGivenTime(timespec_from_ms(t1),tid,its,stop);
+    t2=6000;
+    n2=executeFunctionForGivenTime(timespec_from_ms(t2),tid,its,stop);
+    *a=(n2-n1)/(t2-t1);
+    *b=n2-(*a)*t2;
+    timer_delete(tid);
+}
+
+
+
 void exC()
 {
     double counter = 0.;
-    volatile bool stop = false; 
+    volatile bool otherStop = false; 
 
     double a=0;
     double b=0;
@@ -74,10 +76,11 @@ void exC()
     std::cout << "a = " << a <<" b = " << b << ".\n";
 
     clock_gettime(CLOCK_REALTIME, &debut);
-    incr(22000, &counter, &stop);
+    incr(22000, &counter, &otherStop);
     clock_gettime(CLOCK_REALTIME, &fin);
 
     duree=fin-debut;
     std::cout << "duree = " << timespec_to_ms(duree) <<" nombre suppose de boucle  = " << a*timespec_to_ms(duree)+b << ".\n";
 
 }
+#include "ex_b.h"
