9493cdb303511d1beacd138c055c164cda10bbc9
Etienne <etienne@DESKTOP-IMIIADJ.localdomain>
2019-03-19 08:54:58 +0100

    Added Mutex and Thread headers

---
+++ td4/Thread.h
@@ -0,0 +1,28 @@
+#ifndef __TD4_THREAD_H
+#define __TD4_THREAD_H
+
+#include "PosixThread.h"
+
+class Thread : public PosixThread
+{
+    public:
+        Thread();
+        ~Thread();
+
+        void start(void);
+
+        double startTime_ms(void);
+        double stopTime_ms(void);
+        double execTime_ms(void);
+
+        static void sleep_ms(double delay_ms);
+
+    protected:
+        void run(void);
+
+    private:
+        static void * call_run(void * v_thread):
+
+}
+
+#endif  //__TD4_THREAD_H
